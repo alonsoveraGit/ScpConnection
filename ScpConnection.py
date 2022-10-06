5e0be0d669c6718e6f0c31bbe24fd30853654d95
alonsoveraGit <alonsoverajimenez@gmail.com>
2022-10-06 18:50:44 +0200

    added sshpass on Password mode

--- ScpConnection.py
+++ ScpConnection.py
@@ -4,25 +4,17 @@ from os import system
 def clear():
     system("clear")
 
-#Default for testing
-ip="192.168.58.128"
-origen = "/home/alonso/ejemplo.txt"
-user = "alonso"
-destino = "/home/alonso/"
-idfile = "/home/alonso/alonso"
-port = "22"
-modo = "1"
 #Needs Questions
 
-print("---------------------------")
+print("--------------------------")
 print("    1.Con Key Privada")
 print("    2.Con Password")
-print("---------------------------")
+print("--------------------------")
 modo = input("Que modo usar =  [1]  :    ")
 
 clear()
 
-ip= input("IP =   Ej: 192.168.58.244  :      ")
+ip= input("IP =   Ej: 192.168.58.128  :      ")
 clear()
 
 origen = input("Ruta de lo que deseas pasar =   Ej: /home/alonso/ejemplo.txt :      ")
@@ -39,14 +31,15 @@ clear()
 
 port = input("Puerto para conexión, Ej: [22]    :")
 clear()
-
+password = input("Contraseña,     :")
 
 if modo == "1":
     idfile = input("Introduce la ruta de tu KEY Privada   Ej: /home/alonso/alonso  :     ")
     system(f"scp -i {idfile} -p{port} {origen} {user}@{ip}:{destino}")
 
 elif modo == "2":
-    system(f"scp -p{port} {origen} {user}@{ip}:{destino}")
+    #system(f"scp -p{port} {origen} {user}@{ip}:{destino}")
+    system(f"sshpass -p {password} scp {origen} {user}@{ip}:{destino}")
 
 else:
     print ("Seleccione una opción valida [1-2]")
